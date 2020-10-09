10e27e30e83613c403c91ea8652602a01a3adcee
domw324 <domw324@gmail.com>
2020-10-10 00:16:33 +0900

    cupertino Tap app Structure

---
+++ coffee_street/lib/resource/cupertino/model/cup_app_state_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AppStateModel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SizedBox(),
+    );
+  }
+}
