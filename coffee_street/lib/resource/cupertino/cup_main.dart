614071b80f12508d4071e6747dd4c3829e76f929
domw324 <domw324@gmail.com>
2020-10-11 20:52:56 +0900

    cupertino 상태관리 추가

--- coffee_street/lib/resource/cupertino/cup_main.dart
+++ coffee_street/lib/resource/cupertino/cup_main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'cup_app.dart';
+import 'model/cup_app_state_model.dart';
 
 void main() {
-  return runApp(CupertinoStoreApp());
+  return runApp(
+    ChangeNotifierProvider<AppStateModel>(
+      create: (_) => AppStateModel()..loadProducts(),
+      child: CupertinoStoreApp(),
+    )
+  );
 }
--- coffee_street/lib/resource/cupertino/model/cup_app_state_model.dart
+++ coffee_street/lib/resource/cupertino/model/cup_app_state_model.dart
@@ -1,6 +1,34 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart' as foundation;
 
-class AppStateModel extends StatelessWidget {
+import 'cup_product.dart';
+import 'cup_product_repository.dart';
+
+double _salesTaxRate = 0.06;
+double _shippingCostPerItem = 7;
+
+class AppStateModel extends foundation.ChangeNotifier {
+  List<Product> _availableProducts;
+  Category _selectedCategory = Category.all;
+  final _productsInCart = <int, int>{};
+
+  Map<int, int> get productsInCart {}
+  int get totalCartQuantity {}
+  Category get selectedCategory {}
+  double get subtotalCost {}
+  double get shippingCost {}
+
+  double get tax {}
+  double get totalCost {}
+  List<Product> getProducts() {}
+  List<Product> search(String searchTerms) {}
+  void addProductToCart(int productId) {}
+  void removeItemFromCart(int productId) {}
+  Product getProductById(int id) {}
+  void clearCart() {}
+  void loadProducts() {}
+  void setCategory(Category newCategory) {}
+
   @override
   Widget build(BuildContext context) {
     return Container(
